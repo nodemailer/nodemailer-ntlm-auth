f77ae92fcef6575a138985ae2a0312b94deb0730
Andris Reinman <andris@kreata.ee>
2019-01-09 22:48:24 +0200

    v1.0.0

---
+++ README.md
@@ -0,0 +1,46 @@
+# NTLM authentication for Nodemailer
+
+Nodemailer 5+ allows to use custom authentication mechanisms. While there is no support in Nodemailer for NTLM then it can be provided with an addon.
+
+> **NB!** Experimental! Might not work as advertised due to the lack of not being actually able to use any real NTLM capable server
+
+## Install
+
+Requires Nodejs v8.0.0 or newer
+
+```
+npm install nodemailer-ntlm-auth
+```
+
+## Usage
+
+```
+const nodemailer = require('nodemailer');
+const nodemailerNTLMAuth = require('nodemailer-ntlm-auth');
+
+let transporter = nodemailer.createTransport({
+    host: 'smtp.example.com',
+    port: 465,
+    secure: true,
+    auth: {
+        type: 'custom',
+        method: 'NTLM',
+        user: 'username',
+        pass: {
+            domain: 'my-domain',
+            workstation: 'my-desktop',
+            pass: 'verysecret'
+        }
+    },
+    customAuth: {
+        NTLM: nodemailerNTLMAuth
+    }
+});
+
+transporter.sendMail({
+    from: 'sender@example.com',
+    to: 'recipient@example.com',
+    subject: 'hello world!',
+    text: 'hello!'
+}, console.log)
+```
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const ntlm = require('httpntlm/ntlm');
+
+module.exports = async ctx => {
+    let cfg = ctx.auth.credentials.pass || {};
+    let authMessage = ntlm.createType1Message({
+        domain: cfg.domain || '',
+        workstation: cfg.workstation || ''
+    });
+
+    let cmd;
+
+    cmd = await ctx.sendCommand('AUTH ' + authMessage);
+    if (cmd.status !== 334) {
+        throw new Error('Invalid login sequence while waiting for server challenge string');
+    }
+
+    let challengeString = cmd.text;
+    if (!/^NTLM/i.test(challengeString)) {
+        challengeString = 'NTLM ' + challengeString;
+    }
+
+    let type2Message = ntlm.parseType2Message(challengeString, err => {
+        // parseType2Message handles callback synchronously to return an error response
+        throw err;
+    });
+
+    let type3Message = ntlm.createType3Message(type2Message, {
+        domain: cfg.domain || '',
+        workstation: cfg.workstation || '',
+        username: ctx.auth.credentials.user,
+        password: cfg.pass
+    });
+
+    type3Message = type3Message.substring(5); // remove the "NTLM " prefix
+
+    cmd = await ctx.sendCommand(type3Message);
+    if (cmd.status !== 235) {
+        throw new Error('Invalid login sequence while waiting for "235"');
+    }
+
+    // authenticated!
+    return true;
+};
